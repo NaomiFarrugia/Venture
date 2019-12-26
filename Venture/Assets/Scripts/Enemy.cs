a8e4c8492c2fc4f8347ee49e5695ba8de85ea6e2
NaomiFarrugia <naomi-farrugia-@hotmail.com>
2019-12-26 11:13:53 +0100

    Enemy

---
+++ Venture/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3f;
+
+    private Submarine _Submarine; 
+
+    private Animator _animator;
+
+    void Start()
+    {
+        _Submarine = GameObject.Find("Submarine").GetComponent<Submarine>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    // move left at 3m/s
+    transform.Translate(Vector3.left * _speed * Time.deltaTime);
+    }
+}
